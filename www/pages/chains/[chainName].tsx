9828dbeb5f33ed012335fdbb5848a4a90ab57715
Dan Lynch <pyramation@gmail.com>
2024-05-18 05:04:38 -0700

    build

--- www/pages/chains/[chainName].tsx
+++ www/pages/chains/[chainName].tsx
@@ -1,3 +1,7 @@
+import { chains } from '@chain-registry/v2/mainnet';
+import { GetStaticPaths,GetStaticProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+
 import { Layout } from "@/components";
 
 import ChainHeader from '../../components/common/ChainHeader';
@@ -9,3 +13,31 @@ export default function Components() {
     </Layout>
   );
 }
+
+interface QueryParams extends ParsedUrlQuery {
+  chainName: string;
+}
+
+interface ChainAssetListPage {
+  chainName: string;
+}
+
+const SKIP = ['andromeda1', 'thorchain'];
+
+export const getStaticPaths: GetStaticPaths<QueryParams> = async () => {
+  const paths = chains.filter(a=>!SKIP.includes(a.chainName)).map(({ chainName }) => ({ params: { chainName } }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+
+export const getStaticProps: GetStaticProps<ChainAssetListPage, QueryParams> = async (context) => {
+  const { chainName } = context.params!;
+  return {
+    props: {
+      chainName
+    },
+  };
+};
